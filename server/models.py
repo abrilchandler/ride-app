9c524452454838f0f8d21507314e32ce3d6cd9b9
Chandler Abril <abrilchandler1@gmail.com>
2025-01-08 13:31:37 -0700

    commit

--- server/models.py
+++ server/models.py
@@ -15,17 +15,6 @@ class User(db.Model, SerializerMixin):
 
     rides = db.relationship('Ride', secondary='bookings', back_populates='users')
     
-   # rides = db.relationship('Ride', back_populates='user')
-# this is a direct relationship, but i need a through relationship
-# my user has many items, and a bucket has many items, so a user has many buckets through items
-    
-    #rides  db.relationship('Ride, secondary='booking')
-    # user has many bookings through rides?
-
-    # user has many rides through booking
-    # i should be able to get models by myself i think. I more so need what i just texteed you for the front end
-
-    #my rides should be sent rides that i have booked using the button on available rides
 
     def __repr__(self):
         return f'<User {self.id}, {self.username}>'
@@ -36,16 +25,12 @@ class Ride(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-   # user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     pickup_time = db.Column(db.DateTime, nullable=False)
     spaces = db.Column(db.Integer, nullable=False, default=1)
     destination = db.Column(db.String, nullable=False)
     duration = db.Column(db.Integer, nullable=False)
     mileage = db.Column(db.Integer, nullable=False)
 
-    # Relationship to User (one-to-many)
-    # user = db.relationship('User', back_populates='rides')
-        # Many-to-many relationship through 'Booking' model
     users = db.relationship('User', secondary='bookings', back_populates='rides')
 
 
